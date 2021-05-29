ebb3cd935f5cffc74a322c57eb9dc24faee441cf
Wilhelm Ågren <wilhelmagren98@gmail.com>
2021-05-29 12:35:01 +0200

    rewrote serialization for parser

--- lib/state.py
+++ lib/state.py
@@ -8,7 +8,7 @@ import numpy as np
 import torch
 from net import resNet
 
-
+"""
 class Evaluator(object):
     def __init__(self):
         weights = torch.load('../nets/value_old.pth', map_location=lambda storage, loc: storage)
@@ -19,15 +19,15 @@ class Evaluator(object):
         board = state.serialize()[None]
         output = self.model(torch.tensor(board).float())
         return float(output.data[0][0])
+"""
 
 
 class State(object):
     def __init__(self, board=None):
-        weights = torch.load('../nets/value.pth', map_location=lambda storage, loc: storage)
-        self.model = resNet()
-        self.model.load_state_dict(weights)
+        # weights = torch.load('../nets/value.pth', map_location=lambda storage, loc: storage)
+        # self.model = resNet()
+        # self.model.load_state_dict(weights)
         self.board = chess.Board() if board is None else board
-        self.evaluator = Evaluator()
         self.piecemap = {}
         self.bitmap = self.serialize()
 
@@ -36,20 +36,20 @@ class State(object):
 
     def serialize(self) -> np.array:
         """
-        8x8x12 bitmap representation of board. Extremely sparse. CNN favours sparse data...
+        7x8x8 bitmap representation of board. Extremely sparse. CNN favours sparse data...
         """
 
-        bitmap = np.zeros(shape=(8*8, 12))
-
+        bitmap = np.zeros(shape=(7, 8, 8))
+        CLR_MOVE = {'b': -1, 'w': 1}
         for idx in range(8*8):
             piece = self.board.piece_at(idx)
             if piece is not None:
                 onehot_offset = {"P": 0, "N": 1, "B": 2, "R": 3, "Q": 4, "K": 5,
-                                 "p": 6, "n": 7, "b": 8, "r": 9, "q": 10, "k": 11}[piece.symbol()]
-                bitmap[idx, onehot_offset] = 1
+                                 "p": 0, "n": 1, "b": 2, "r": 3, "q": 4, "k": 5}[piece.symbol()]
+                bitmap[onehot_offset, idx % 8, math.floor(idx / 8)] = 1 if piece.symbol().isupper() else -1
+                bitmap[6, idx % 8, math.floor(idx / 8)] = 1*CLR_MOVE[self.board.fen().split(' ')[1]]
 
         self.update_map()
-        bitmap = np.reshape(bitmap, (12, 8, 8))
         return bitmap
 
     def update_map(self):
@@ -67,9 +67,7 @@ class State(object):
         return list(self.board.legal_moves)
 
     def value(self) -> float:
-        board = self.serialize()[None]
-        output = self.model(torch.tensor(board).float())
-        return float(output.data[0][0])
+        return 0
 
     def __repr__(self):
         return self.board.__str__()
