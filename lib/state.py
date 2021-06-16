2622dd5fa69df8da19c6b2069b6e5994787a5301
Bund <wilhelmagren98@gmail.com>
2021-06-16 19:09:56 +0200

    rewrite of everything, serialization is differnt. now 11x8x8 bitmap representation

--- lib/state.py
+++ lib/state.py
@@ -1,108 +1,99 @@
 """
-Author: Wilhelm Ågren, wagren@kth.se
-Last edited: 28/05-2021
+Author: Wilhelm Ågren
+Last edited: 16/06-2021
+
+State class wrapper for chess.Board instance.
 """
-import chess
+
+
 import math
+import chess
 import numpy as np
-import torch
-from net import TinyChessNet, TinyPruneNet
-
-"""
-class Evaluator(object):
-    def __init__(self):
-        weights = torch.load('../nets/value_old.pth', map_location=lambda storage, loc: storage)
-        self.model = resNet()
-        self.model.load_state_dict(weights)
-
-    def __call__(self, state):
-        board = state.serialize()[None]
-        output = self.model(torch.tensor(board).float())
-        return float(output.data[0][0])
-"""
 
 
 class State(object):
     def __init__(self, board=None):
-        weights_prune = torch.load('../nets/tiny_class_value.pth', map_location=lambda storage, loc: storage)
-        weights_reg = torch.load('../nets/tiny_value_R.pth', map_location=lambda storage, loc: storage)
-        self.model_prune = TinyPruneNet()
-        self.model_reg = TinyChessNet()
-        self.model_prune.load_state_dict(weights_prune)
-        self.model_reg.load_state_dict(weights_reg)
+        self.bitmap_shape = (12, 8, 8)
         self.board = chess.Board() if board is None else board
-        self.piecemap = {}
-        self.bitmap = self.serialize()
 
-    def set_board(self, board):
-        self.board = board
+    def __float__(self) -> float:
+        return float(self.value())
 
+    def __str__(self) -> str:
+        return str(self.board)
+
+    def __abs__(self) -> float:
+        return abs(self.value())
+
+    def __round__(self, n=None) -> float:
+        return round(self.value(), 2) if n is None else round(self.value(), n)
+
+    def __floor__(self) -> float:
+        return math.floor(self.value())
+
+    def __ceil__(self) -> float:
+        return math.ceil(self.value())
+
+    def __ge__(self, other) -> bool:
+        return self.value() >= other.value()
+
+    def __le__(self, other) -> bool:
+        return self.value() <= other.value()
+
+    def __gt__(self, other) -> bool:
+        return self.value() > other.value()
+
+    def __lt__(self, other) -> bool:
+        return self.value() < other.value()
+
+    def __eq__(self, other) -> bool:
+        return round(self.value()) == round(other.value())
+
+    def __ne__(self, other) -> bool:
+        return round(self.value()) != round(other.value())
+
+    #| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
     def serialize(self) -> np.array:
         """
-        7x8x8 bitmap representation of board. Extremely sparse. CNN favours sparse data...
+        bit(s) 0-5: 'one-hot' encoding of the piece at the square, positive for white pieces and negative for black
+        bit 6: player turn to move, 1 for white -1 for black
+        bit 7: long or short castle available for current player (bit 7), 0 or 1
+        bit 8: legal en passant available, 0 or 1
+        bit 9: promotion available, 0 or 1
+        bit 10: king is in check, 0 or 1
+
+        Last 5 bits greatly impacts the outcome for the board, and those features should be learned more easily due to
+        the increased loss during training(?). Is this too complex for my generated data? 11x8x8 = 704bits, maybe not...
         """
-
-        bitmap = np.zeros(shape=(7, 8, 8))
-        CLR_MOVE = {'b': -1, 'w': 1}
-        for idx in range(8*8):
+        bitmap, c2m, fen = np.zeros(shape=self.bitmap_shape), {'b': -1, 'w': 1}, self.board.fen()
+        to_move, castles_available = c2m[fen.split(' ')[1]], self.board.has_castling_rights(self.board.turn)
+        enpassant_available, legal_moves = self.board.has_legal_en_passant(), list(self.board.legal_moves)
+        promotion_available = not all(list(move.promotion is None for move in legal_moves))
+        is_check, piece_offset = self.board.is_check(), {"P": 0, "N": 1, "B": 2, "R": 3, "Q": 4, "K": 5,
+                                                         "p": 0, "n": 1, "b": 2, "r": 3, "q": 4, "k": 5}
+        for idx in range(64):
+            x_idx, y_idx = idx % 8, math.floor(idx / 8)
             piece = self.board.piece_at(idx)
-            if piece is not None:
-                onehot_offset = {"P": 0, "N": 1, "B": 2, "R": 3, "Q": 4, "K": 5,
-                                 "p": 0, "n": 1, "b": 2, "r": 3, "q": 4, "k": 5}[piece.symbol()]
-                bitmap[onehot_offset, idx % 8, math.floor(idx / 8)] = 1 if piece.symbol().isupper() else -1
-                bitmap[6, idx % 8, math.floor(idx / 8)] = 1*CLR_MOVE[self.board.fen().split(' ')[1]]
+            if piece:
+                # not an empty square, set first 1-6 bits according to piece color & type
+                bitmap[piece_offset[piece.symbol()], x_idx, y_idx] = 1 if piece.symbol().isupper() else -1
+            bitmap[6, x_idx, y_idx] = to_move
+            bitmap[7, x_idx, y_idx] = 1*castles_available
+            bitmap[8, x_idx, y_idx] = 1*enpassant_available
+            bitmap[9, x_idx, y_idx] = 1*promotion_available
+            bitmap[10, x_idx, y_idx] = 1*is_check
 
-        self.update_map()
         return bitmap
 
-    def update_map(self):
-        self.piecemap.clear()
-        for idx in range(8*8):
-            piece = self.board.piece_at(idx)
-            if piece is not None:
-                if piece.symbol() not in self.piecemap:
-                    self.piecemap.update({piece.symbol(): [(idx % 8, 7 - math.floor(idx / 8))]})
-                else:
-                    self.piecemap[piece.symbol()] += [(idx % 8, 7 - math.floor(idx / 8))]
+    @staticmethod
+    def value() -> float:
+        return 0.0
 
-    def branches(self) -> list:
-        # Generator function board.legal_moves/0
-        return list(self.board.legal_moves)
 
-    def best_moves(self, num_moves=3):
-        c2i = {
-            'a': 0,
-            'b': 1,
-            'c': 2,
-            'd': 3,
-            'e': 4,
-            'f': 5,
-            'g': 6,
-            'h': 7
-        }
-        board = self.serialize()[None]
-        soft = torch.nn.Softmax(dim=1)
-        output = soft(self.model_prune(torch.tensor(board).float()))
-        _, indices = torch.topk(output, num_moves, largest=True)
-        from_move_idx, bestmoves = [], []
-        for idx in indices[0]:
-            from_move_idx.append(idx)
-        for move in self.branches():
-            moveaslist = list(move.uci())
-            for idx in list(map(int, from_move_idx)):
-                if c2i[moveaslist[0]] == (idx % 8) and int(moveaslist[1]) == (math.ceil(idx / 8)):
-                    bestmoves.append(move)
-        return bestmoves
-
-    def value(self) -> float:
-        board = self.serialize()[None]
-        output = self.model_reg(torch.tensor(board).float())
-        return float(output.data[0][0])
-
-    def __repr__(self):
-        return self.board.__str__()
+def main():
+    s = State()
+    s.serialize()
 
 
 if __name__ == '__main__':
-    s = State()
-    print(s)
+    main()
