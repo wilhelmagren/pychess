847b28f6ebef2c243b9532dee170b86b934c231f
Bund <wilhelmagren98@gmail.com>
2021-06-22 13:12:32 +0200

    classifier training and validation

--- lib/net.py
+++ lib/net.py
@@ -7,8 +7,6 @@ Runs with CUDA 11.0 using #!usr/bin/python, not python3!!!!
 
 
 import os
-import time
-import chess
 import torch
 import numpy as np
 import torch.nn as nn
@@ -28,12 +26,15 @@ class ChessClassifier(nn.Module):
         self.l3 = nn.Linear(in_features=512, out_features=256)
         self.l4 = nn.Linear(in_features=256, out_features=3)
         self.dropout = nn.Dropout(p=0.25)
+        self.bn1 = nn.BatchNorm1d(num_features=1024)
+        self.bn2 = nn.BatchNorm1d(num_features=512)
+        self.bn3 = nn.BatchNorm1d(num_features=256)
 
     def forward(self, x):
         x = x.view(-1, 19*8*8)
-        x = self.dropout(F.relu(self.l1(x)))
-        x = self.dropout(F.relu(self.l2(x)))
-        x = self.dropout(F.relu(self.l3(x)))
+        x = self.dropout(F.relu(self.bn1(self.l1(x))))
+        x = self.dropout(F.relu(self.bn2(self.l2(x))))
+        x = self.dropout(F.relu(self.bn3(self.l3(x))))
         x = F.relu(self.l4(x))
         return x
 
@@ -52,7 +53,7 @@ class Data(Dataset):
 class DataGen:
     def __init__(self, categorical=False):
         X, Y = self.dataloader(categorical)
-        self.datasets = self.split(X, Y, 0.2)
+        self.datasets = {'test': Data(X, Y)}  # self.split(X, Y, 0.2)
         # self.histogram()
 
     def histogram(self):
@@ -75,7 +76,9 @@ class DataGen:
     def dataloader(categorical):
         X, Y = [], []
         for file in os.listdir('../parsed/'):
-            if file.__contains__('{}_BIG'.format('_C' if categorical else '_R')):
+            # if file.__contains__('_TEST'):
+            #     continue
+            if file.__contains__('{}_BIG_TEST'.format('_C' if categorical else '_R')):
                 print(' | parsing data from filepath {}'.format(file))
                 data = np.load(os.path.join('../parsed/', file), allow_pickle=True)
                 X.append(data['arr_0'])
@@ -97,14 +100,32 @@ class DataGen:
         return datasets
 
 
-def plothistory(t, v):
-    plt.plot([x for x in range(len(t))], t, color='maroon', label='training loss')
-    plt.plot([x for x in range(len(v))], v, color='navy', label='validation loss')
+def plothistory(tl, vl, ta, va):
+    plt.plot([x for x in range(len(tl))], tl, color='royalblue', label='training', linewidth=1.2)
+    plt.plot([x for x in range(len(vl))], vl, color='forestgreen', label='validation', linewidth=1.2)
     plt.title('loss evolution')
     plt.xlabel('epoch')
-    plt.ylabel('MSE loss')
+    plt.ylabel('cross entropy')
     plt.legend()
     plt.show()
+    plt.plot([x for x in range(len(ta))], ta, color='royalblue', label='training', linewidth=1.2)
+    plt.plot([x for x in range(len(va))], va, color='forestgreen', label='validation', linewidth=1.2)
+    plt.title('accuracy evolution')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.show()
+
+
+def test(model, testdata):
+    acc = 0
+    model.load_state_dict(torch.load('../nets/ChessClassifierBN.pth', map_location=lambda storage, loc: storage))
+    for batch_idx, (sample, label) in enumerate(testdata):
+        sample, label = sample.to(device).float(), label.to(device).long()
+        output = model(sample)
+        _, predicted = torch.max(output.data, 1)
+        acc += (predicted == label).sum().item() / output.shape[0]
+    print(' | testing accuracy: {:.1f}%'.format(100*acc/len(testdata)))
 
 
 if __name__ == '__main__':
@@ -117,17 +138,21 @@ if __name__ == '__main__':
     model.to(device)
     summary(model, (1, 1216))
     optimizer, floss = optim.Adam(model.parameters(), lr=1e-4), nn.CrossEntropyLoss()
+    testdata = DataLoader(data.datasets['test'], batch_size=4096, shuffle=True)
+    """
     traindata = DataLoader(data.datasets['train'], batch_size=8192, shuffle=True)
     validdata = DataLoader(data.datasets['valid'], batch_size=4096, shuffle=False)
     model.train()
-    starttime, thist, vhist = time.time(), [], []
+    starttime, tlhist, tahist, vlhist, vahist = time.time(), [], [], [], []
     for epoch in range(50):
-        tloss, vloss = 0, 0
+        tloss, tacc, vloss, vacc = 0, 0, 0, 0
         for batch_idx, (sample, label) in enumerate(traindata):
             # Performing backprop after each training batch
             sample, label = sample.to(device).float(), label.to(device).long()
             optimizer.zero_grad()
             output = model(sample)
+            _, predicted = torch.max(output.data, 1)
+            tacc += (predicted == label).sum().item()/output.shape[0]
             loss = floss(output, label)
             loss.backward()
             optimizer.step()
@@ -136,12 +161,18 @@ if __name__ == '__main__':
         for batch_idx, (sample, label) in enumerate(validdata):
             sample, label = sample.to(device).float(), label.to(device).long()
             output = model(sample)
+            _, predicted = torch.max(output.data, 1)
+            vacc += (predicted == label).sum().item()/output.shape[0]
             loss = floss(output, label)
             vloss += loss.item()
-        thist.append(tloss / len(traindata))
-        vhist.append(vloss / len(validdata))
-        print(' | epoch\t{},\ttloss\t{:.3f}\tvloss\t{:.3f}'.format(epoch + 1, tloss / len(traindata), vloss / len(validdata)))
+        tlhist.append(tloss / len(traindata))
+        tahist.append(tacc / len(traindata))
+        vlhist.append(vloss / len(validdata))
+        vahist.append(vacc / len(validdata))
+        print(' | ep {:02d},  tloss {:.2f}   vloss {:.2f},  tacc {:.1f}%   vacc {:.1f}%'.format(epoch + 1, tloss / len(traindata), vloss / len(validdata), 100*tacc / len(traindata), 100*vacc / len(validdata)))
     print('-|' + '-' * 62)
     print(' | done! Training took {:.0f}s'.format(time.time() - starttime))
-    torch.save(model.state_dict(), "../nets/ChessClassifier.pth")
-    plothistory(thist, vhist)
+    torch.save(model.state_dict(), "../nets/ChessClassifierNoBN.pth")
+    plothistory(tlhist, vlhist, tahist, vahist)
+    """
+    test(model, testdata)
