59a262d6091963b0a159c4d134b5690562cf1025
Wilhelm Ågren <36638274+willeagren@users.noreply.github.com>
2021-06-22 20:09:58 +0200

    Update torchtrainer.py

--- lib/torchtrainer.py
+++ lib/torchtrainer.py
@@ -6,9 +6,12 @@ when using pytorch library.
 
 class TorchTrainer:
     """
-    Requires a pytorch nn.
+    Requires a pytorch nn model, path to datasets, problem to solve either classification/regression.
     """
     def __init__(self, model, datapath, problem, **kwargs):
         self.model, self.datapath, self.problem = model, datapath, problem
         self.regularization = kwargs.get['regularization']
         self.visualize = kwargs.get['visualize']
+    
+    
+    
