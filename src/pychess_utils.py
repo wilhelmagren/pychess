1a925d91b3a5069a745c4398f5258d75ce5039ca
ulysses@DESKTOP-3TBH7B3 <wagren@kth.se>
2021-10-10 12:30:28 +0200

    utlity file with helper functions, default global values etc

---
+++ src/pychess_utils.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+
+DEFAULT_WHITE		= 'white'
+DEFAULT_BLACK		= 'black'
+DEFAULT_TIME		= 300
+DEFAULT_INCREMENT	= 5
+
+
+def VPRINT(msg, verbose):
+	print(msg) if verbose else None
+
+
+
