b461d7c6056b19c143fa47b0f0d128e5cf84b49a
wagren <wagren@kth.se>
2021-10-11 23:05:43 +0200

    :star: :alien: print buffer implemented as StdOutWrapper for TUI instances

--- src/utils.py
+++ src/utils.py
@@ -10,9 +10,37 @@ WHITE_TO_PLAY		= "Whites move: "
 BLACK_TO_PLAY		= "Blacks move: "
 
 
-def WPRINT(msg, tpe, verbose):
-	print("[*]  {}  ".format(tpe) + msg) if verbose else None
 
+class StdOutWrapper:
+	"""
+	"""
+	def __init__(self):
+		self._text = ""
+
+	def put(self, msg):
+		"""
+		"""
+		self._text += "\n" + msg
+
+	def write(self):
+		"""
+		"""
+		for row in self._text.split("\n"):
+			print(row)
+
+
+"""
+"""
+def WSTRING(msg, tpe, verbose):
+	return "[*]  {}  {}".format(tpe, msg) if verbose else None
+
+
+def WPRINT(msg, tpe, verbose):
+	print("[*]  {}  {}".format(tpe, msg)) if verbose else None
+
+
+def ESTRING(msg, tpe):
+	return "[!]  {}  {}".format(tpe, msg)
 
 def EPRINT(msg, tpe):
-	print("[!]  {}  ".format(tpe) + msg)
+	print("[!]  {}  {}".format(tpe, msg))
