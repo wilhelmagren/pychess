5fcda778997057f08121aa12281d1c7fb2507dbe
ulysses@bund-subsystem <wagren@kth.se>
2021-10-12 21:24:18 +0200

    inheriting from class PychessMode and cleaning up :herb: :cactus:

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,18 @@
-from .game import PychessGame
+"""
+PychessGUI
+
+Author: Wilhelm Ågren, wagren@kth.se
+Last edited: 12-10-2021
+"""
+from .mode      import PychessMode
+from .game      import PychessGame
+from .utils     import *
 
 
-class PychessGUI:
-	def __init__(self):
-		self.mode = 'gui'
+class PychessGUI(PychessMode):
+    def __init__(self):
+        super().__init__(players, names, verbose=verbose, mode='gui')
 
-	def start(self):
-		print("[*]  starting pychess instace with mode {}".format(self.mode))
+
+    def start(self):
+        print("[*]  starting pychess instance with mode {}".format(self.mode))
