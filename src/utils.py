893a9f96656298c81b590a8a7acc60e27afed6fd
ulysses@bund-subsystem <wagren@kth.se>
2021-10-12 21:56:43 +0200

    rewrite StdOutWrapper :grin: :dragon: :cactus:

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,22 @@
+"""
+PychessUtils file implementing diverse utility functions
+used in various situations and instances of other Pychess*
+instantiations. default values variables are set here
+and the StdOutWrapper for PychessMode=TUI is also implemented
+here as it is necessary for both PychessTUI and PychessGame.
+
+Author: Wilhelm Ågren, wagren@kth.se
+Last edited: 12-10-2021
+"""
 import os
 import sys
 
 
+
+"""!!! global variables
+used in pychess.py when creating PychessMode
+and user has not supplied all optional args.
+"""
 DEFAULT_WHITE		= 'white'
 DEFAULT_BLACK		= 'black'
 DEFAULT_TIME		= 300
@@ -10,22 +25,29 @@ WHITE_TO_PLAY		= "Whites move: "
 BLACK_TO_PLAY		= "Blacks move: "
 
 
-
 class StdOutWrapper:
-    """
-    ye this is the stdoutwrapper
+    """!!! definition for class StdOutWrapper
+    class is used for printing to terminal whenever
+    PychessMode=TUI. the text-buffer is built up
+    during program runtime until it terminates at which
+    point the buffer is printed to standard output.
     """
     def __init__(self):
         self._text = ""
 
-    def put(self, msg):
-        """
-        """
-        if msg is None:
-            return
-        self._text += msg + "\n"
 
-    def write(self):
+    def WPUT(self, msg, tpe, verbose):
+        string = "[*]  {}  {}\n".format(tpe, msg)
+        if verbose:
+            self._text += string
+
+
+    def EPUT(self, msg, tpe):
+        string = "[!]  {}  {}\n".format(msg, tpe)
+        self._text += string
+
+
+    def WRITE(self):
         """
         """
         rows = self._text.split("\n")
@@ -33,18 +55,25 @@ class StdOutWrapper:
             print(row) if row_idx != len(rows) - 1 else None
 
 
-"""
+
+"""!!! global funcs
+WSTRING(str, str, bool) =>  str
+ESTRING(str, str)       =>  str
+WPRINT(str, str, bool)  =>  none
+EPRINT(str, str)        =>  none
 """
 def WSTRING(msg, tpe, verbose):
     return "[*]  {}  {}".format(tpe, msg) if verbose else None
+
+
+def ESTRING(msg, tpe):
+    return "[!]  {}  {}".format(tpe, msg)
 
 
 def WPRINT(msg, tpe, verbose):
     print("[*]  {}  {}".format(tpe, msg)) if verbose else None
 
 
-def ESTRING(msg, tpe):
-    return "[!]  {}  {}".format(tpe, msg)
-
 def EPRINT(msg, tpe):
     print("[!]  {}  {}".format(tpe, msg))
+
