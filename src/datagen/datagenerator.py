63128e0fbc935cf155f26861b49c5eac146b1355
ulysses@bund-subsystem <wagren@kth.se>
2021-10-16 22:21:30 +0200

    threaded reading of PGN files for generating regression values related to positional FEN strings. located in 2019_blitz_FEN-R.npz :herb: :alien:

--- src/datagen/datagenerator.py
+++ src/datagen/datagenerator.py
@@ -24,6 +24,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 from utils import WPRINT
 
 FILEPATHS = os.listdir('../../data/pgn-data/')
+EXTREME_VALUES = [-6000, 6000]
+
 
 def num_games_in_pgn(pgnfile):
     with open('../../data/pgn-data/'+pgnfile+'/'+pgnfile) as pgn:
@@ -47,39 +49,62 @@ class DataGenerator:
         self._categorical   = kwargs.get('categorical', False)
         self._regression    = kwargs.get('regression', False)
 
-        assert self._categorical != self._regression, print("can't do both regresssion and categorical labels...")
-    
+        assert self._categorical != self._regression, EPRINT("can't do both regresssion and categorical labels...", str(self))
+
+
     def __str__(self):
         return "DataGenerator"
 
+
     def plot(self):
-        for key in self._store.keys():
-            vals = self._store[key].values()
-            plt.hist(vals, bins=10000, color='gray', edgecolor='black', linewidth='1')
-            plt.title('Regression label distribution')
-            plt.xlabel('label')
-            plt.ylabel('num samples')
-            plt.xlim((-10000, 10000))
-            plt.savefig('{}-dist.png'.format(key))
+        vals = self._store.values()
+        plt.hist(vals, bins=13000, color='gray', edgecolor='black', linewidth='1')
+        plt.title('Regression label distribution')
+        plt.xlabel('label')
+        plt.ylabel('num samples')
+        plt.xlim((-6500, 6500))
+        plt.savefig('{}-dist.png'.format('2019'))
 
     
-    def export_store(self):
-        WPRINT("exporting table with FEN+labels", str(self), True)
-        completedict = self._merge_thread_dicts()
-        np.savez_compressed('{}_blitz_FEN-{}'.format(self._filepath.split('_')[1], 'C' if self._categorical else 'R'), completedict)
+    def import_data(self):
+        WPRINT("importing data dictionary with FEN+labels...", str(self), True)
+        completedict = dict()
+        files = list(f for f in os.listdir('.') if '.npz' in f)
+        dicts = list(map(lambda f: np.load(f, allow_pickle=True)['arr_0'], files))
+        for dic in dicts:
+            completedict = {**completedict, **dic[()]}
+        WPRINT("done importing", str(self), True)
+        self._store = completedict
+
     
+    def export_data(self):
+        WPRINT("exporting data ditctionary with FEN+labels", str(self), True)
+        completedict = self._merge_thread_dicts()
+        np.savez_compressed('{}_blitz_FEN-{}'.format(self._filepath.split('_')[1], 'C' if self._categorical else 'R'), self._store)
+
+
+    def scale_data(self):
+        WPRINT("scaling down extreme points in data", str(self), True)
+        scaleddict = {k: v if v < EXTREME_VALUES[1] else EXTREME_VALUE[1] for k, v in self._store.items()}
+        scaleddict = {k: v if v > EXTREME_VALUES[0] else EXTREME_VALUES[0] for k, v in scaleddict.items()}
+
+        assert len(scaleddict) == len(self._store), EPRINT("missed values when scaling data, dictionaries not matching size", str(self))
+
+        self._store = scaleddict
+        WPRINT("done scaling down data", str(self), True) 
+
 
     def _merge_thread_dicts(self):
-        dics, completedict = list(self._store[key] for key in self._store.keys()), {}
+        WPRINT("merging the thread-created dictionaries", str(self), True)
+        dics, completedict = list(self._store[key] for key in self._store.keys()), dict()
         for dic in dics:
             completedict = {**completedict, **dic}
-
         return completedict
 
 
     def _parse(self, threadid, offset):
         engine = chess.engine.SimpleEngine.popen_uci('../../stockfish/stockfish_14_x64_avx2.exe')
-        thread_store, n_games, n_pos = {}, 0, 0
+        thread_store, n_games, n_pos = dict(), 0, 0
         with open('../../data/pgn-data/'+self._filepath+'/'+self._filepath) as pgn:
             WPRINT("thread={} skipping {} games".format(threadid, offset), str(self), True)
             for _ in range(offset):
@@ -102,11 +127,11 @@ class DataGenerator:
                             score = score.score()
                         else:
                             if score.mate() > 0:
-                                score = 10000
+                                score = EXTREME_VALUES[1]
                             elif score.mate() < 0:
-                                score = -10000
+                                score = EXTREME_VALUES[0]
                             else:
-                                score = 10000 if to_move < 0 else -10000
+                                score = EXTREME_VALUES[1] if to_move < 0 else EXTREME_VALUES[0]
                         thread_store[t_fen] = score
                         n_pos += 1
                 n_games +=1
@@ -114,7 +139,6 @@ class DataGenerator:
         engine.close()
         WPRINT("thread={} done!".format(threadid), str(self), True)
         self._store[threadid] = thread_store
-
 
 
     def t_generate(self):
@@ -127,12 +151,15 @@ class DataGenerator:
         for proc in procs:
             proc.join()
         WPRINT("all threads done!", str(self), True)
+        self._store = self._merge_thread_dicts()
 
 
 
 if __name__ == "__main__":
-    datagen = DataGenerator(FILEPATHS[0], numgames=2, regression=True)
-    datagen.t_generate()
-    # datagen.plot()
-    datagen.export_store()
+    datagen = DataGenerator(FILEPATHS[0], threads=10, numgames=10000, regression=True)
+    datagen.import_data()
+    datagen.scale_data()
+    #datagen.t_generate()
+    datagen.plot()
+    #datagen.export_data()
 
